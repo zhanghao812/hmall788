b01f7bbd9951af40c48cc3ce63fc62005a663049
zhanghao <4250834153@qq.com>
2024-08-08 18:27:03 +0800

    bug修改 401

--- hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
@@ -48,7 +48,7 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
 		//3.获取token
 		String token =null;
 		List<String> headers = request.getHeaders().get("authorization");
-		if(CollUtils.isEmpty(headers)){
+		if(!CollUtils.isEmpty(headers)){
 			//headers!=null &&!headers.isEmpty()
 			token = headers.get(0);
 		}
